c0c6180bdd3a04ce4af6be0e8287c20165b1a107
LandrumTrev <landrum.trevillian@gmail.com>
2018-11-01 14:14:11 -0400

    friends dev setup

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,47 +10,18 @@
 
 // CONFIRM CONNECTION
 // =====================================================
-console.log("friends.js connected");
-
-
-// DEPENDENCIES (NPM MODULES)
-// =============================================================
-var express = require("express");
-var path = require("path");
-
-
-// MODULE IMPORTS (ROUTES and DATA)
-// =============================================================
-
-
-// EXPRESS SERVER CONFIG
-// =============================================================
-var app = express();
-
-
-// EXPRESS SERVER MIDDLEWARE
-// =============================================================
-// allow Express server to accept URL encoded data,
-// extended to also allow URL encoded JSON objects and arrays
-app.use(express.urlencoded({ extended: true }));
-
-// allow Express server to accept JSON data (for POSTs)
-app.use(express.json());
-
-// allow Express server to server static files from PUBLIC directory
-app.use(express.static('public'));
-
-
+// console.log("friends.js connected");
 
 
 // DATA
 // =====================================================
 
-var friends = [
-    {
-        "name":"McKenzie",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+// array to hold compatibility data of all users in "database"
+// no actual database; the array in "friends.js" is the database
+var friends = [{
+        "name": "McKenzie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             3,
             2,
             1,
@@ -61,12 +32,12 @@ var friends = [
             1,
             5,
             4
-          ]
-      },
-      {
-        "name":"Bubba",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name": "Bubba",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             5,
             4,
             3,
@@ -77,12 +48,12 @@ var friends = [
             3,
             2,
             1
-          ]
-      },
-      {
-        "name":"Arkentootz",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name": "Arkentootz",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             5,
             1,
             4,
@@ -93,35 +64,123 @@ var friends = [
             5,
             4,
             1
-          ]
-      }
+        ]
+    },
+    {
+        "name": "Camden",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            4,
+            5,
+            5,
+            3,
+            3,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Alyssa",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            2,
+            2,
+            3,
+            1,
+            5,
+            2,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "HAL 9000",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            3,
+            4,
+            5,
+            1,
+            3,
+            3,
+            3,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Mork from Ork",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            4,
+            1,
+            2,
+            1,
+            3,
+            4,
+            5,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "Babs",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            3,
+            2,
+            3,
+            5,
+            4,
+            5,
+            2,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "Knuckles Johnson",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            4,
+            3,
+            3,
+            1,
+            5,
+            2,
+            4,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Alex",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            1,
+            2,
+            3
+        ]
+    }
 ];
 
 
 // MODULE EXPORT
 // =====================================================
 module.exports = friends;
-
-
-// INSTRUCTIONS
-// =====================================================
-// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
-
-// json
-
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
